8509509b67aca5a558c42721ca88b3c5161de00a
Lisias <me@lisias.net>
2020-10-07 01:38:06 -0300

    Updating Legal Mambo Jambo

--- Source/StageRecovery/RecoveryControllerWrapper.cs
+++ Source/StageRecovery/RecoveryControllerWrapper.cs
@@ -1,19 +1,20 @@
 ﻿/*
-    This file is part of StageRecovery
+    This file is part of Stage Recovery /L
+    © 2020 LisiasT
     © 2014-2018 magico13
 
-    StageRecovery licensed as follows:
+    Stage Recovery /L licensed as follows:
 
     * GPL 3.0 : https://www.gnu.org/licenses/gpl-3.0.txt
 
     And you are allowed to choose the License that better suit your needs.
 
-    StageRecovery is distributed in the hope that it will be useful,
+    Stage Recovery /L is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 
     You should have received a copy of the GNU General Public License 3.0
-    along with StageRecovery If not, see <https://www.gnu.org/licenses/>.
+    along with Stage Recovery /L. If not, see <https://www.gnu.org/licenses/>.
 */
 using System;
 using System.Collections.Generic;
