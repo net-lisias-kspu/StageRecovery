e9624932f2c017ceb0d4e3645e09f18ba50ed512
Lisias <me@lisias.net>
2020-10-08 11:45:00 -0300

    Properly inserting the Add'On the the MM Patch Cycles.

---
+++ Source/StageRecovery/ModuleManagerSupport.cs
@@ -0,0 +1,30 @@
+﻿/*
+    This file is part of Stage Recovery /L
+    © 2020 LisiasT
+
+    Stage Recovery /L licensed as follows:
+
+    * GPL 3.0 : https://www.gnu.org/licenses/gpl-3.0.txt
+
+    And you are allowed to choose the License that better suit your needs.
+
+    Stage Recovery /L is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+    You should have received a copy of the GNU General Public License 3.0
+    along with Stage Recovery /L. If not, see <https://www.gnu.org/licenses/>.
+*/
+using System.Collections.Generic;
+
+namespace StageRecovery
+{
+	public static class ModuleManagerSupport
+	{
+		public static IEnumerable<string> ModuleManagerAddToModList()
+		{
+			string[] r = {typeof(ModuleManagerSupport).Namespace};
+			return r;
+		}
+	}
+}
